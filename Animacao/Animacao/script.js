075c1a0e9cddb576fe47e6abf2ae3e25c48a5d1e
SophiaTaddeoN <sophiataddeon@gmail.com>
2025-04-15 14:05:16 -0300

    animaçao corrigido

--- Animacao/Animacao/script.js
+++ Animacao/Animacao/script.js
@@ -3,48 +3,37 @@ let ctx1 = canvas1.getContext('2d');
 
 // EX AULA
 let kOKUJOUDAI = {
-  x: 0,
-  y: 100,
-  raio: 50,
+  x: 150,
+  y: 150,
+  width: 100,
+  height: 120,
   img: new Image(),
   desenha: function() {
-    
-    if (this.img.complete && this.img.naturalWidth > 0) {
-      ctx1.drawImage(this.img, this.x, this.y, 2*this.raio, 2*this.raio);
-    }
+    ctx1.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 }
 
 
-kOKUJOUDAI.img.src = "IMAGEM/kOKUJOUDAI.png"; 
+kOKUJOUDAI.img.src = "IMAGEM/kOKUJOUDAI.png";
+kOKUJOUDAI.img.onload = function() {
+  animacao(); 
+};
 
-function desenhar() {
+function animacao() {
   ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
   kOKUJOUDAI.desenha(); 
-  requestAnimationFrame(desenhar);
+  requestAnimationFrame(animacao);
 }
-
-
-desenhar();
 
 document.addEventListener("mousemove", function(evento) {
   let rect = canvas1.getBoundingClientRect();
   let x_mouse = evento.clientX - rect.left;
   let y_mouse = evento.clientY - rect.top;
 
-  if (x_mouse < canvas1.width - 2*kOKUJOUDAI.raio && x_mouse > 0) {
-    kOKUJOUDAI.x = x_mouse;
-  } else if (x_mouse >= canvas1.width - 2*kOKUJOUDAI.raio) {
-    kOKUJOUDAI.x = canvas1.width - 2*kOKUJOUDAI.raio;
-  } else {
-    kOKUJOUDAI.x = 0;
-  }
+  let novo_x = x_mouse - kOKUJOUDAI.width/2;
+  let novo_y = y_mouse - kOKUJOUDAI.height/2;
+  
 
-  if (y_mouse < canvas1.height - 2*kOKUJOUDAI.raio && y_mouse > 0) {
-    kOKUJOUDAI.y = y_mouse;
-  } else if (y_mouse >= canvas1.height - 2*kOKUJOUDAI.raio) {
-    kOKUJOUDAI.y = canvas1.height - 2*kOKUJOUDAI.raio;
-  } else {
-    kOKUJOUDAI.y = 0;
-  }
+  kOKUJOUDAI.x = Math.max(0, Math.min(canvas1.width - kOKUJOUDAI.width, novo_x));
+  kOKUJOUDAI.y = Math.max(0, Math.min(canvas1.height - kOKUJOUDAI.height, novo_y));
 });
