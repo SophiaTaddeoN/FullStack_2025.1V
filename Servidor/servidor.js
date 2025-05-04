ba9844aa69431db42fe9c05c5ec4f4730c0679be
SophiaTaddeoN <sophiataddeon@gmail.com>
2025-05-04 15:09:04 -0300

    BLOG

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,89 +1,88 @@
 require("colors");
 const http = require("http");
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const uri =
+  "mongodb+srv://sophiataddeonasc:George20@mongodb.kfnurgf.mongodb.net/blog";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
-const users = []; // Banco de dados em memória
-
-const uri = 'mongodb+srv://sophiataddeonasc:<db_password>@mongodb.kfnurgf.mongodb.net/?retryWrites=true&w=majority&appName=MongoDB'
-
-// Configurações
-app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", "./views");
 
-// Rotas
-app.get("/", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "public", "Aula_02", "Projeto_parte_2", "index.html")
-  );
-});
+app.use(express.static(__dirname + "/public"));
 
-app.get("/login", (req, res) => {
-  res.sendFile(
-    path.join(
-      __dirname,
-      "public",
-      "Aula_02",
-      "Projeto_parte_2",
-      "Aula_09",
-      "login.html"
-    )
-  );
-});
+let posts; // Coleção de posts
+const postsCollection = client.db("blog").collection("posts");
 
-app.get("/cadastro", (req, res) => {
-  res.sendFile(
-    path.join(
-      __dirname,
-      "public",
-      "Aula_02",
-      "Projeto_parte_2",
-      "Aula_09",
-      "cadastro.html"
-    )
-  );
-});
+client
+  .connect()
+  .then(() => {
+    const db = client.db("blog");
+    posts = db.collection("posts");
 
-app.post("/cadastro", (req, res) => {
-  const { nome, login, senha, nascimento } = req.body;
-  users.push({ nome, login, senha, nascimento });
+    // Rota principal: redireciona para Projects.html
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/public/Aula_02/Projeto_parte_2/project.html");
+    });
 
-  res.render("resposta", {
-    nome,
-    login,
-    nasc: nascimento,
-    status: "Cadastro realizado com sucesso!",
+    // Rota para exibir o blog com posts dinâmicos
+    app.get("/blog", async (req, res) => {
+      try {
+        const todos = await posts.find().toArray();
+        res.render("blogs", { posts: todos }); // usa blogs.ejs
+      } catch (err) {
+        res.status(500).send("Erro ao buscar posts.");
+      }
+    });
+
+    // Rota para exibir o formulário de cadastro
+    app.get("/cadastrar_post.html", (req, res) => {
+      res.sendFile(
+        __dirname +
+          "/public/Aula_02/Projeto_parte_2/Aula_10/Cadastrar_post.html"
+      );
+    });
+
+    // Rota para receber e cadastrar novo post
+    app.post("/cadastrar", async (req, res) => {
+      const { titulo, resumo, conteudo } = req.body;
+      if (!titulo || !resumo || !conteudo) {
+        return res.status(400).send("Preencha todos os campos.");
+      }
+
+      try {
+        await posts.insertOne({ titulo, resumo, conteudo });
+        // Após cadastrar, redireciona para a página de confirmação
+        res.render("post_cadastrado", { titulo });
+      } catch (err) {
+        res.status(500).send("Erro ao cadastrar post.");
+      }
+    });
+
+    // Iniciar servidor na porta 80
+    http.createServer(app).listen(80, () => {
+      console.log("Servidor rodando na porta 80...".rainbow);
+    });
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao MongoDB:", err);
   });
-});
 
-app.post("/login", (req, res) => {
-  const { login, senha } = req.body;
-  const user = users.find((u) => u.login === login && u.senha === senha);
-
-  if (user) {
-    res.render("resposta", {
-      nome: user.nome,
-      login: user.login,
-      nasc: user.nascimento,
-      status: "Login realizado com sucesso!",
-    });
-  } else {
-    res.render("resposta", {
-      nome: "",
-      login: "",
-      nasc: "",
-      status: "Login falhou! Verifique usuário e senha.",
-    });
-  }
-});
-
-// Iniciar servidor
-const PORT = 80;
-http.createServer(app).listen(PORT, () => {
-  console.log(`Servidor rodando: http://localhost`.rainbow);
-});
+// app.get("/resetar-posts", async (req, res) => {
+//   try {
+//     await postsCollection.deleteMany({});
+//     res.send("Todos os posts foram apagados.");
+//   } catch (erro) {
+//     res.status(500).send("Erro ao apagar os posts.");
+//   }
+// });
